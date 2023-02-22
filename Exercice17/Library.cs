798f4ec8484c715bc02126b5989e7e370230059e
Pierre Poulin <ppoulin@cegep-ste-foy.qc.ca>
2023-02-22 15:27:43 -0500

    Initial commit

---
+++ Exercice17/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Exercice17
+{
+    public class Library
+    {
+        // TODO Créer la fonction "Sum".
+
+        // TODO Créer la fonction "Average".
+
+        // TODO Créer la fonction "Max".
+
+        // TODO Créer la fonction "FilterEven".
+
+
+    }
+}
